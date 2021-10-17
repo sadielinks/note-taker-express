f54c2220c7f505f10c9a23342a5efb11d861e8e2
Sadie Sial <sadiesial@gmail.com>
2021-10-16 20:10:24 -0500

    worked on delete with ids

--- server.js
+++ server.js
@@ -47,10 +47,16 @@ app.post('/api/notes', (req, res) => {
     res.json(writeThatNote); 
 });
 
-// DELETE uses id to remove
+// DELETE uses id to remove, worked with TA - will delete + edit notes thru this larger function
 app.delete('/api/notes/:id', function (req, res){
-  let dataThatBase = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
-
+  let dataThatBase = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  // assign variable for finding thru ID
+  let notesID = req.params.id;
+  // new notes to db.json
+  newNoteData = dataThatBase.filter((myNoteNow) => {
+    return myNoteNow.id != notesID;
+  })
+  
 });
 
 
